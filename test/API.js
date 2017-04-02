9dfaffcd4a85f6b73420beb706f76b4e5e201456
Shane Weickum <sweick23@outlook.com>
2017-04-01 17:47:11 -0700

    update

--- test/API.js
+++ test/API.js
@@ -37,6 +37,63 @@ it('should get BlogPosts on GET', function(){
         })
     });
 });
+it('should add a BlogPost on POST', function(){
+  const newBlog = {title: 'sunny day', content: 'one sunny day', author: 'shane weickum'};
+  return chai.request(app)
+  .post('/BlogPosts')
+  .send(newBlog)
+  .then(function(res){
+    res.should.have.status(201);
+    res.should.be.json;
+    res.body.should.be.a('object');
+    res.body.should.include.keys('id', 'title', 'content', 'author');
+    res.body.id.should.not.be.null;
+
+    res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+
+  });
+});
+
+it('should update BlogPosts on PUT', function(){
+
+
+  const updateData = {
+    title: 'saturday morning',
+    content: 'this saturday morning I woke up with my kids, had coffee and turned on saturday morning cartoons.',
+    author: 'Shane Weickum'
+  };
+
+  return chai.request(app)
+  .get('/BlogPosts')
+  .then(function(res){
+    updateData.id = res.body[0].id;
+
+
+    return chai.request(app)
+    .put(`/BlogPosts/${updateData.id}`)
+    .send(updateData);
+  })
+
+  .then(function(res){
+    res.should.have.status(200);
+    res.should.be.json;
+    res.body.should.be.a('object');
+    res.body.should.deep.equal(updateData);
+  });
+});
+
+it('should delete BlogPosts on DELETE', function(){
+  return chai.request(app)
+
+  .get('/BlogPosts')
+  .then(function(res){
+    return chai.request(app)
+    .delete(`/BlogPosts/${res.body[0].id}`);
+  })
+  .then(function(res) {
+    res.should.have.status(204);
+      });
+  });
 });
 
 
