35341d3bdcae002ddcf71953b42adfc179d76a52
Shane Weickum <sweick23@outlook.com>
2017-04-02 22:55:40 -0700

    update

--- test/API.js
+++ test/API.js
@@ -6,31 +6,31 @@ const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
 
-chai.use('http');
+chai.use(chaiHttp);
 
-describe(BlogPosts, function(){
+describe('BlogPosts', function(){
 
 
 before(function(){
-    return runServer();
+  return runServer();
 });
 
 
 
 after(function(){
-    return closeServer();
+  return closeServer();
 });
 
 it('should get BlogPosts on GET', function(){
     return chai.request(app)
-    .get('/BlogPosts')
+    .get('/apiRouter')
     .then(function(res){
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('array');
         res.body.length.should.be.at.least(1);
 
-        const expectedKeys = ["id","title", "content", "author"];
+        const expectedKeys = ["id","title", "content", "author", "publishDate"];
         res.body.forEach(function(item){
             item.should.be.a('object');
             item.should.include.keys(expectedKeys);
@@ -40,16 +40,16 @@ it('should get BlogPosts on GET', function(){
 it('should add a BlogPost on POST', function(){
   const newBlog = {title: 'sunny day', content: 'one sunny day', author: 'shane weickum'};
   return chai.request(app)
-  .post('/BlogPosts')
+  .post('/apiRouter')
   .send(newBlog)
   .then(function(res){
     res.should.have.status(201);
     res.should.be.json;
     res.body.should.be.a('object');
-    res.body.should.include.keys('id', 'title', 'content', 'author');
+    res.body.should.be.all.keys('id', 'title', 'content', 'author', 'publishDate');
     res.body.id.should.not.be.null;
 
-    res.body.should.deep.equal(Object.assign(newRecipe, {id: res.body.id}));
+    res.body.should.deep.equal(Object.assign(newBlog, {id: res.body.id, publishDate: res.body.publishDate}));
 
   });
 });
@@ -57,43 +57,51 @@ it('should add a BlogPost on POST', function(){
 it('should update BlogPosts on PUT', function(){
 
 
-  const updateData = {
-    title: 'saturday morning',
-    content: 'this saturday morning I woke up with my kids, had coffee and turned on saturday morning cartoons.',
-    author: 'Shane Weickum'
-  };
-
   return chai.request(app)
-  .get('/BlogPosts')
+  .get('/apiRouter')
   .then(function(res){
-    updateData.id = res.body[0].id;
+    
+function saturdayMorning(){
+    return 'every saturday morning i wake up and have coffee and turn on the saturday morning cartoons for my kids'
+}
 
-
+  const updateData = Object.assign(res.body[0], {
+    title: 'saturday morning',
+    content: saturdayMorning()
+    
+    
+  });
+    
+  });
     return chai.request(app)
-    .put(`/BlogPosts/${updateData.id}`)
-    .send(updateData);
-  })
-
+    .put(`/apiRouter/${res.body[0].id}`)
+    .send(updateData)
+ 
   .then(function(res){
-    res.should.have.status(200);
+    res.should.have.status(204);
     res.should.be.json;
     res.body.should.be.a('object');
     res.body.should.deep.equal(updateData);
+  
   });
 });
 
 it('should delete BlogPosts on DELETE', function(){
   return chai.request(app)
 
-  .get('/BlogPosts')
+  .get('/apiRouter')
   .then(function(res){
     return chai.request(app)
-    .delete(`/BlogPosts/${res.body[0].id}`);
+    .delete(`/apiRouter/${res.body[0].id}`);
   })
   .then(function(res) {
     res.should.have.status(204);
       });
   });
-});
+}); 
+
+
+
+
 
 
